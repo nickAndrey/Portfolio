fc19f299a11742452b3d8bc3e401c39ea5a21e2b
andrezej <a.husiev@fudosecurity.com>
2021-06-01 18:28:14 +0200

    Add content for `About` page.

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,3 +1,22 @@
+import styled from "styled-components";
+import { device } from "../../breakpoints";
+
+const AboutContent = styled.p`
+  max-width: 80%;
+  @media ${device.mobileS} {
+    font-size: 2em;
+  }
+  @media ${device.laptopL} {
+    font-size: 4em;
+  }
+`;
+
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <AboutContent>
+      I’m a Front End Developer specialise in single-page dynamic web
+      applications. I enjoy creating new things from small business web-sites to
+      huge interactive web applications, always open to new opportunities.
+    </AboutContent>
+  );
 }
