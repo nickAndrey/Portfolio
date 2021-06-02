0f0e55f5d06d65ad1ba01ba44fbb2eb54ad0469b
andrezej <a.husiev@fudosecurity.com>
2021-06-02 10:17:13 +0200

    Add container box for represent information.

---
+++ src/components/services/service-container.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { device } from "../../breakpoints";
+
+const ServiceContainerLayout = styled.div`
+  h3 {
+    margin-bottom: 20px;
+    font-weight: 600;
+    @media ${device.mobileS} {
+      font-size: 1.3em;
+    }
+    @media ${device.tablet} {
+      font-size: 1.5em;
+    }
+    @media ${device.laptopL} {
+      font-size: 2em;
+    }
+  }
+  p {
+    font-weight: 300;
+    @media ${device.mobileS} {
+      font-size: 1em;
+    }
+    @media ${device.tablet} {
+      font-size: 1.3em;
+    }
+    @media ${device.laptopL} {
+      font-size: 1.5em;
+    }
+  }
+`;
+
+export default function ServiceContainer({ title, content }) {
+  return (
+    <ServiceContainerLayout>
+      <h3>{title}</h3>
+      <p>{content}</p>
+    </ServiceContainerLayout>
+  );
+}
